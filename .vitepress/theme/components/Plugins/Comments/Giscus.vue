e16c7af4972a97b4272db67b1093c8384e01fbb1
zhengxiaopeng <zxpscau@163.com>
2026-04-13 18:26:12 +0800

    Add new images, redirect page, robots.txt, and Vercel configuration
    - Added multiple image files including logos and screenshots.
    - Created a new redirect.html page for handling URL redirection with safety checks.
    - Added a robots.txt file to manage web crawler access.
    - Introduced a Vercel configuration file for deployment settings.

---
+++ .vitepress/theme/components/Plugins/Comments/Giscus.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="giscusWrapper">
+    <div class="giscus" :class="['comment-content', 'twikoo']"></div>
+  </div>
+</template>
+
+<script setup>
+
+
+onMounted(() => {
+  // 创建 script 元素
+  const script = document.createElement('script');
+  script.src = 'https://giscus.app/client.js';
+  script.setAttribute('data-repo', 'zxpsuper/zxpsuper.github.io');
+  script.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnk5NTMzMzM0OQ==');
+  script.setAttribute('data-category', 'Announcements');
+  script.setAttribute('data-category-id', 'DIC_kwDOBa6r5c4CevP1');
+  script.setAttribute('data-mapping', 'pathname');
+  script.setAttribute('data-strict', '0');
+  script.setAttribute('data-reactions-enabled', '1');
+  script.setAttribute('data-emit-metadata', '0');
+  script.setAttribute('data-input-position', 'bottom');
+  script.setAttribute('data-theme', 'https://gw.alipayobjects.com/os/k/t/comment.css');
+  script.setAttribute('data-lang', 'zh-CN');
+  script.crossOrigin = 'anonymous';
+  script.async = true;
+  document.querySelector('.giscus').appendChild(script);
+});
+</script>
