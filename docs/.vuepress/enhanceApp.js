28b77117a61641f9b8c349ef35ce85283691ac4e
zhengxiaopeng <zxpscau@163.com>
2026-05-20 10:08:27 +0800

    feat: 增加51统计

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -18,6 +18,31 @@ export default ({
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(hm, s);
       })();
+      !(function (p) {
+        "use strict";
+        !(function (t) {
+          var s = window,
+            e = document,
+            i = p,
+            c = "".concat(
+              "https:" === e.location.protocol ? "https://" : "http://",
+              "suporka.site/js/js-sdk-pro.min.js",
+            ),
+            n = e.createElement("script"),
+            r = e.getElementsByTagName("script")[0];
+          (n.type = "text/javascript"),
+            n.setAttribute("charset", "UTF-8"),
+            (n.async = !0),
+            (n.src = c),
+            (n.id = "LA_COLLECT"),
+            (i.d = n);
+          var o = function () {
+            s.LA.ids.push(i);
+          };
+          s.LA ? s.LA.ids && o() : ((s.LA = p), (s.LA.ids = []), o()),
+            r.parentNode.insertBefore(n, r);
+        })();
+      })({ id: "3PzSZE57tRx5JUqt", ck: "3PzSZE57tRx5JUqt", hashMode: true });
     }
     import("./components/element-ui").then((elementUi) => {
       Vue.use(elementUi);
@@ -28,31 +53,5 @@ export default ({
         }
       }, 0);
     });
-
-    !(function (p) {
-      "use strict";
-      !(function (t) {
-        var s = window,
-          e = document,
-          i = p,
-          c = "".concat(
-            "https:" === e.location.protocol ? "https://" : "http://",
-            "suporka.site/js-sdk-pro.min.js",
-          ),
-          n = e.createElement("script"),
-          r = e.getElementsByTagName("script")[0];
-        (n.type = "text/javascript"),
-          n.setAttribute("charset", "UTF-8"),
-          (n.async = !0),
-          (n.src = c),
-          (n.id = "LA_COLLECT"),
-          (i.d = n);
-        var o = function () {
-          s.LA.ids.push(i);
-        };
-        s.LA ? s.LA.ids && o() : ((s.LA = p), (s.LA.ids = []), o()),
-          r.parentNode.insertBefore(n, r);
-      })();
-    })({ id: "3PzSZE57tRx5JUqt", ck: "3PzSZE57tRx5JUqt", hashMode: true });
   }
 };
