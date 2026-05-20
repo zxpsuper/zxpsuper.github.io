b4171cd992a6de81c827fd9a7da49e6812474d61
zhengxiaopeng <zxpscau@163.com>
2026-05-20 09:49:16 +0800

    feat: 增加51统计

--- docs/.vuepress/components/tongji.vue
+++ docs/.vuepress/components/tongji.vue
@@ -9,5 +9,7 @@
         s.parentNode.insertBefore(hm, s)
       })()
     </script>
+    <script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+    <script>LA.init({id:"3PzSZE57tRx5JUqt",ck:"3PzSZE57tRx5JUqt"})</script>
   </div>
 </template>
